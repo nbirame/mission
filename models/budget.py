62c6ccc2d2e662646b6c38f22ee74d8380115416
nbirame <biramendiaye71@gmail.com>
2025-02-18 12:21:15 +0000

    optimisation code

--- models/budget.py
+++ models/budget.py
@@ -2,5 +2,6 @@ from odoo import models, fields
 
 class Budget(models.Model):
     _name = "mission.budget"
+    _description = "Imputation bugetaire"
 
     name = fields.Integer(string="Imputation budgétaire")
